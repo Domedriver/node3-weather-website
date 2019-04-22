ba73297333c6a0fc4e7288ff57b51527759e1909
Domedriver <mlaman@gmail.com>
2019-04-22 13:30:52 -0400

    changed text of forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -15,8 +15,9 @@ const forecast = (longitude, latitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location.', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ` It is currently ${body.currently.temperature} degrees out. And there is a ${body.currently.precipProbability * 100}% 
-            chance of rain. The high temperature for the day is expected to be ${body.daily.data[0].temperatureHigh} with a low of ${body.daily.data[0].temperatureLow} degrees.`)
+            callback(undefined, body.daily.data[0].summary + ` It is currently ${Math.round(body.currently.temperature)} 
+            degrees outside with a ${body.currently.precipProbability * 100}% chance of rain today. The high temperature for the day 
+            is expected to be ${Math.round(body.daily.data[0].temperatureHigh)} with a low of ${Math.round(body.daily.data[0].temperatureLow)}.`)
         }
     })
 }
